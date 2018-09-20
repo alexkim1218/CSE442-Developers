8141e34ee4a97b5c48de41e99fdd0d340c89563b
hding3 <43121959+hding3@users.noreply.github.com>
2018-09-20 16:26:32 -0400

    Database assignment(loginsystem) for Jas

---
+++ db.py
@@ -0,0 +1,27 @@
+
+
+class logInSystem:
+	
+	def __init__(self):
+		pass
+####################################################
+	#check if user name exist
+	#return true if exist
+
+	def checkUsrName(self, usr):
+
+		return false
+####################################################
+	#insert username and passwod to UserTb
+	#return true if successfully signed up
+
+	def signUp(self, usr, pwd):
+
+		return false
+####################################################
+	#verify the username and password
+	#return true if username and password correct
+
+	def logIn(self, usr, pwd):
+
+		return false
