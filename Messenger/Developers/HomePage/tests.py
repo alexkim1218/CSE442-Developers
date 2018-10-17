d3df06d18289fb1f8dc466f27fb9731a66c4a039
hding3 <43121959+hding3@users.noreply.github.com>
2018-10-17 14:33:31 -0400

    unit test for three query functions #31
    test for login, signup, homepageview
    3 tests passed 3

--- Messenger/Developers/HomePage/tests.py
+++ Messenger/Developers/HomePage/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from HomePage import main_db
+from HomePage.models import ProfileTb
 
-# Create your tests here.
+class DatabaseTestCase(TransactionTestCase):
+    def setUp(self):
+        ProfileTb.objects.create(username='testusr', password='testpwd', userstory='my name is test', firstname='testfirstname', lastname='testlastname', email='test@test.com', education='UB', dateofbirth='11201995')
+        # main_db.SubmitSignUp('testusr', 'testpwd','my name is test','testfirstname', 'testlastname','test@test.com','testUB','11201995')
+    def test_login(self):
+        dic = {}
+        self.assertEqual(type(main_db.logIn('testusr', 'testpwd')), type(dic))
+    def test_homepageview(self):
+        dic = {}
+        self.assertEqual(type(main_db.homepgView('testusr')), type(dic))
+    def test_signup(self):
+        self.assertEqual(main_db.SubmitSignUp('testusr2', 'testpwd','my name is test','testfirstname', 'testlastname','test@test.com','testUB','11201995'), True)
