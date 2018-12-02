da643bbc13fcee97397fc671a44ba78d9a14d6dc
seyaduab <seyaduab@buffalo.edu>
2018-12-02 17:15:11 -0500

    Viewing Friend information #52
    What I did:
    -Made updates to last sprint by adding named clickers.
    -Added function to render the User information for Database
    -Changed the overall template of the page.
    -Created routers to redirct user to the blog page of the friend selected.

--- friends/urls.py
+++ friends/urls.py
@@ -1,7 +1,7 @@
+
 from django.urls import path
 from friends import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('', views.index, name = 'index')
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,26 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.http import HttpResponseRedirect
-from library.usermodel import User
-from friends.models import ProfileTb, PhotoTb
-from library import databasecheck
 
-class UserPageView(TemplateView):
-    template_name = "displayfriends.html"
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from home import db_functions
+from home import views
 
 
 def index(request):
-        if request.GET.get('addfriend'):
-            if databasecheck.addFriend(User.getusername(User), request.GET['search']):
-                print('I am here')
-                User.setfriend_list(User, databasecheck.friendsList(User.getusername(User)))
-                return render(request, 'displayfriends.html', {'friend_list': User.getfriend_list(User)})
-            else:
-                return HttpResponseRedirect('/friend_list/')
-        elif request.GET.get('bth'):
-            return HttpResponseRedirect('/userpage/')
+    global sfriendn
+    if request.GET.get('bth'):
+        return HttpResponseRedirect('/userpage/')
+
+    if request.GET.get('add'):
+        if db_functions.addFriend(views.getusername(), request.GET['search']):
+            return render(request, 'friends.html', {'friend_list': db_functions.getFriend(views.getusername())})
         else:
-            return render(request, 'displayfriends.html', {'friend_list': User.getfriend_list(User)})
+            return HttpResponseRedirect('/friend/')
+
+    if request.GET.get('friendB'):
+        sfriendn = request.GET['friendB']
+        msg_list = db_functions.getuserstory(sfriendn)
+        dictF = db_functions.homepgView(sfriendn)
+        return render_to_response('friend_blog.html', {'pname': request.GET['friendB'], 'gender': views.gender(sfriendn),
+                                    'msg_lists': msg_list, 'currentuser': views.getusername(),
+                                    'fn': dictF['firstname'], 'ln': dictF['lastname'], 'mail': dictF['email'],
+                                    'dob': dictF['dateofbirth'], 'edu': dictF['education'],
+                                        'friend_list': db_functions.getFriend(sfriendn)})
+
+    if request.GET.get('share'):
+        msg_list = db_functions.adduserstory(sfriendn, views.getusername()+": "+request.GET['story'])
+        dictF = db_functions.homepgView(sfriendn)
+        return render_to_response('friend_blog.html',
+                                  {'pname': sfriendn, 'gender': views.gender(sfriendn),
+                                   'msg_lists': msg_list, 'fn': dictF['firstname'], 'ln': dictF['lastname'], 'mail': dictF['email'],
+                                    'dob': dictF['dateofbirth'], 'edu': dictF['education'],
+                                        'friend_list': db_functions.getFriend(sfriendn)})
+    if request.GET.get('btf'):
+        return render_to_response('friends.html', {'friend_list': db_functions.getFriend(views.getusername())})
+
+    return render_to_response('friends.html', {'friend_list': db_functions.getFriend(views.getusername())})
+
+
+def getfriendn():
+    return sfriendn
